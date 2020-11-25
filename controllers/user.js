ace89b09c81c093ac2387795505ad5806a5208c0
Gwen Gyr <gwengyr@protonmail.com>
2020-11-25 16:42:06 +0100

    createUser + login ctrl

--- controllers/user.js
+++ controllers/user.js
@@ -2,6 +2,20 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const models = require('../models');
 
+function generateAccessToken(data) {
+  return jwt.sign(data, process.env.SECRET_TOKEN, { expiresIn: '3600s' });
+}
+
+function verifyEmail(email) {
+  const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,4}$/;
+  return emailRegex.test(email);
+}
+
+function verifyAvatar(avatar) {
+  const avatarRegex = /^\s*data:([a-z]+\/[a-z]+(;[a-z\-]+\=[a-z\-]+)?)?(;base64)?,[a-z0-9\!\$\&\'\,\(\)\*\+\,\;\=\-\.\_\~\:\@\/\?\%\s]*\s*$/i;
+  return avatarRegex.test(avatar);
+}
+
 exports.register = (req, res) => {
   const { email, firstname, lastname, password, bio, avatar } = req.body;
 
@@ -16,11 +30,9 @@ exports.register = (req, res) => {
   ) {
     return res.status(400).json({ message: 'Bad request' });
   }
-  const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,4}$/;
-  if (!emailRegex.test(email)) {
+  if (!verifyEmail(email)) {
     return res.status(400).json({ message: 'Bad request' });
   }
-
   if (password.length < 8) {
     return res.status(400).json({ message: 'Bad request' });
   }
@@ -30,8 +42,7 @@ exports.register = (req, res) => {
   if (avatar !== null && avatar !== undefined && typeof avatar !== 'string') {
     return res.status(400).json({ message: 'Bad request' });
   }
-  const avatarRegex = /^\s*data:([a-z]+\/[a-z]+(;[a-z\-]+\=[a-z\-]+)?)?(;base64)?,[a-z0-9\!\$\&\'\,\(\)\*\+\,\;\=\-\.\_\~\:\@\/\?\%\s]*\s*$/i;
-  if (avatar !== null && avatar !== undefined && !avatarRegex.test(avatar)) {
+  if (avatar !== null && avatar !== undefined && !verifyAvatar(avatar)) {
     return res.status(422).json({ message: 'Unprocessable entity' });
   }
   models.User.findOne({
@@ -46,7 +57,104 @@ exports.register = (req, res) => {
       bcrypt
         .hash(password, 8)
         .then((hash) => {
-          // eslint-disable-next-line no-console
+          models.User.create({ firstname, lastname, email, password: hash, bio, avatar })
+            .then((user) => {
+              const accessToken = generateAccessToken({ email: user.email, id: user.id });
+              return res.status(201).json({ user, accessToken });
+            })
+            .catch(() => {
+              return res.status(400).json({ message: 'Bad request' });
+            });
+        })
+        .catch(() => {
+          return res.status(400).json({ message: 'Bad request' });
+        });
+    }
+  });
+  return true;
+};
+
+exports.login = (req, res) => {
+  const { email, password } = req.body;
+  if (email === undefined || password === undefined) {
+    return res.status(400).json({ message: 'Bad request' });
+  }
+  if (typeof email !== 'string' || typeof password !== 'string') {
+    return res.status(400).json({ message: 'Bad request' });
+  }
+  if (!verifyEmail(email)) {
+    return res.status(400).json({ message: 'Bad request' });
+  }
+  models.User.findOne({
+    where: { email },
+  }).then((existingUser) => {
+    if (existingUser === null) {
+      return res.status(404).json({ message: 'Not found' });
+    }
+    bcrypt
+      .compare(password, existingUser.password)
+      .then((passwordsMatch) => {
+        if (!passwordsMatch) {
+          return res.status(400).json({ message: 'Bad request' });
+        }
+        const accessToken = generateAccessToken({ email: existingUser.email, id: existingUser.id });
+        return res.status(200).json({ user: existingUser, accessToken });
+      })
+      .catch(() => {
+        return res.status(400).json({ message: 'Bad request' });
+      });
+    return true;
+  });
+  return true;
+};
+
+exports.logout = (req, res) => {
+  // récupérer l'accessToken de la requête (req...)
+  // rtfm revoke token jwt
+};
+
+exports.createUser = (req, res) => {
+  const { email, firstname, lastname, password, bio, avatar } = req.body;
+
+  if (email === undefined || firstname === undefined || lastname === undefined || password === undefined) {
+    return res.status(400).json({ message: 'Bad request' });
+  }
+  if (
+    typeof email !== 'string' ||
+    typeof firstname !== 'string' ||
+    typeof lastname !== 'string' ||
+    typeof password !== 'string'
+  ) {
+    return res.status(400).json({ message: 'Bad request' });
+  }
+
+  if (!verifyEmail(email)) {
+    return res.status(400).json({ message: 'Bad request' });
+  }
+  if (password.length < 8) {
+    return res.status(400).json({ message: 'Bad request' });
+  }
+  if (bio !== null && bio !== undefined && typeof bio !== 'string') {
+    return res.status(400).json({ message: 'Bad request' });
+  }
+  if (avatar !== null && avatar !== undefined && typeof avatar !== 'string') {
+    return res.status(400).json({ message: 'Bad request' });
+  }
+  if (avatar !== null && avatar !== undefined && !verifyAvatar(avatar)) {
+    return res.status(422).json({ message: 'Unprocessable entity' });
+  }
+  models.User.findOne({
+    attributes: ['email'],
+    where: { email },
+  }).then((existingUser) => {
+    if (existingUser) {
+      res.status(409).json({
+        error: 'Conflict',
+      });
+    } else {
+      bcrypt
+        .hash(password, 8)
+        .then((hash) => {
           models.User.create({ firstname, lastname, email, password: hash, bio, avatar })
             .then((user) => {
               return res.status(201).json(user);
@@ -63,11 +171,12 @@ exports.register = (req, res) => {
   return true;
 };
 
-exports.login = (req, res) => {};
-exports.logout = (req, res) => {};
-exports.createUser = (req, res) => {};
 exports.updateUser = (req, res) => {};
+
 exports.deleteUser = (req, res) => {};
+
 exports.getAllUsers = (req, res) => {};
+
 exports.getOneUser = (req, res) => {};
+
 exports.getMyUser = (req, res) => {};
--- middleware/auth.js
+++ middleware/auth.js
@@ -3,10 +3,10 @@ const jwt = require('jsonwebtoken');
 module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(' ')[1];
-    const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
-    const { userId } = decodedToken;
-    if (req.body.userId && req.body.userId !== userId) {
-      throw Error('Invalid user ID');
+    const decodedToken = jwt.verify(token, process.env.SECRET_TOKEN);
+    const { id, email } = decodedToken;
+    if (!(id && email)) {
+      throw Error('error');
     } else {
       next();
     }
